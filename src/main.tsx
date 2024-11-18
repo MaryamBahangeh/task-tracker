43302bdae8b40e67c6077a95ad66e07b4b555a71
BijanProgrammer <bijaneisapour@gmail.com>
2024-11-18 12:52:55 +0330

    [UPDATE] font

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
-import "./colors.css";
+import "./styles/colors.css";
 import "./index.css";
 
 import App from "./App.tsx";
